43fb0cb8e3a295937bb21d73e3fd7699af225136
Paolo Bertellini <225184@studenti.unimore.it>
2018-12-05 23:43:31 +0100

    Modifica WFpanel per tentativo scroll bar

--- WFM_v5.0/src/ui/WorkFlowPanel.java
+++ WFM_v5.0/src/ui/WorkFlowPanel.java
@@ -2,6 +2,11 @@ package ui;
 
 import java.awt.Color;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class WorkFlowPanel extends JPanel {
 
@@ -11,7 +16,6 @@ public class WorkFlowPanel extends JPanel {
 		Color background = new Color(255, 242, 204);
 		this.setBackground(background);
 		this.setVisible(true);
-		this.setLayout(null);
 		this.revalidate();
 		this.repaint();
 	}
